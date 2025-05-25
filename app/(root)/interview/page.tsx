07b3d5d5f193bc1e01005063872390e5a31b33af
clipChronicle <disej2354@gmail.com>
2025-05-26 02:23:03 +0530

    implement interview generation

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Agent from "@/components/Agent";
+
+const Page = () => {
+    return (
+        <>
+            <h3>interview Generation</h3>
+
+            <Agent userName="you" userId="user1" type="generate"/>
+        </>
+    )
+}
+export default Page
