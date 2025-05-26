5325cbe0973a9b9b482478991405b8f39c7a144b
clipChronicle <disej2354@gmail.com>
2025-05-26 22:17:39 +0530

    ai agent implementation

--- lib/actions/auth.actions.ts
+++ lib/actions/auth.actions.ts
@@ -21,11 +21,12 @@ export async function signUp(params: SignUpParams) {
         await db.collection('users').doc(uid).set({
             name, email
         })
+
         return {
             success: true,
             message: 'Account created successfully. Please sign in.'
         }
-    } catch (e : any) {
+    } catch (e: any) {
         console.error('Error creating a user', e);
 
         if(e.code === 'auth/email-already-exists') {
@@ -82,27 +83,36 @@ export async function setSessionCookie(idToken: string) {
     })
 }
 
-export async function getGurrentUser(): Promise<User | null> {
+export async function getCurrentUser(): Promise<User | null> {
     const cookieStore = await cookies();
+
     const sessionCookie = cookieStore.get('session')?.value;
+
     if(!sessionCookie) return null;
+
     try {
         const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
 
-        const userRecord = await db.collection('users').doc(decodedClaims.uid).get();
+        const userRecord = await db.
+        collection('users')
+            .doc(decodedClaims.uid)
+            .get();
 
         if(!userRecord.exists) return null;
+
         return {
             ...userRecord.data(),
             id: userRecord.id,
         } as User;
     } catch (e) {
         console.log(e)
+
         return null;
     }
 }
 
 export async function isAuthenticated() {
-    const user = await getGurrentUser();
+    const user = await getCurrentUser();
+
     return !!user;
 }
